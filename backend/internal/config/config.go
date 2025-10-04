3c83b92767da9be0d16bf9d1bf57de0b35472744
Sa-Leonardo <saleonardo3636gmail.com>
2025-10-03 22:03:49 -0300

    feat: add config db

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -1,1 +1,31 @@
 package config
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+type Config struct {
+	Port     string
+	DatabaseURL string
+}
+
+func LoadConfig() Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println(".env não encontrado, usando variáveis do sistema")
+	}
+
+	return Config{
+		Port:       getEnv("PORT", "8080"),
+		DatabaseURL: getEnv("DATABASE_URL", "postgres://user:pass@localhost:5432/db"),
+	}
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
